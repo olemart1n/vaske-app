eaf2a0d269c3c62a88f94120348f6437f5b32ee6
ole martin <olesnoen@gmail.com>
2025-05-02 02:38:43 +0200

    replacing 'filter' with an icon

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -26,10 +26,13 @@ export default component$(() => {
 
   return (
     <>
-      <header class="fixed flex w-full bg-white p-1">
+      <header class="fixed flex w-full justify-evenly bg-white p-1.5 text-lg text-slate-900">
         <a
           href="/"
-          class={"my-auto h-fit " + (loc.url.pathname === "/" && "underline")}
+          class={
+            "my-auto h-fit border border-transparent " +
+            (loc.url.pathname === "/" && "border-b-slate-600")
+          }
         >
           Vaskeliste
         </a>
@@ -37,14 +40,22 @@ export default component$(() => {
         <Link
           href="/pågående-vasker"
           class={
-            "my-auto h-fit " +
-            (loc.url.pathname === "/p%C3%A5g%C3%A5ende-vasker/" && "underline")
+            "my-auto h-fit border border-transparent " +
+            (loc.url.pathname === "/p%C3%A5g%C3%A5ende-vasker/" &&
+              " border-b-slate-600")
           }
         >
           Pågående vasker
         </Link>
         <span class="mx-2 text-xl text-slate-200">|</span>
-        <Link href="/layout-2" class="my-auto h-fit text-slate-500">
+        <Link
+          href="/layout-2"
+          class={
+            "my-auto h-fit p-0.5 " +
+            (loc.url.pathname === "/layout-2/" &&
+              "rounded bg-slate-300 outline-1 outline-sky-300")
+          }
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -63,15 +74,32 @@ export default component$(() => {
             <path d="M3 15h18" />
           </svg>
         </Link>
-
+        <span class="mx-2 text-xl text-slate-200">|</span>
         <Link
           class={
-            "ms-auto flex items-center rounded border-none px-2 " +
-            (loc.url.pathname.includes("filter") ? "outline-2" : "outline-1")
+            "my-auto h-fit p-0.5 " +
+            (loc.url.pathname === "/filter/" &&
+              "rounded bg-slate-300 outline-1 outline-sky-300")
           }
           href="/filter"
         >
-          Filter
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-file-search-icon lucide-file-search"
+          >
+            <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+            <path d="M4.268 21a2 2 0 0 0 1.727 1H18a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v3" />
+            <path d="m9 18-1.5-1.5" />
+            <circle cx="5" cy="14" r="3" />
+          </svg>
         </Link>
       </header>
 
